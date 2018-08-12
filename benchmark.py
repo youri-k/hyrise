6e38e77543a7941d0df5d0757ff21f1361612d3f
Leander Neiß <leander.neiss@student.hpi.de>
2018-08-12 19:03:27 +0200

    Add telegram notification to benchmark.py.

--- benchmark.py
+++ benchmark.py
@@ -5,6 +5,17 @@ from datetime import datetime
 import subprocess
 from subprocess import run
 
+import requests
+import urllib
+
+def bot_sendtext(bot_message):
+    encoded_message = urllib.parse.quote_plus(bot_message)
+    bot_token = '606667730:AAHrpRwxIYfhhV8GrbYHGgKvGqSBVjHMZMw'
+    bot_chatID = '5505853'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + encoded_message
+
+    requests.get(send_text)
+
 CWD = os.getcwd()
 RESULT_DIR_BASE = 'results'
 EXECUTABLE = 'build-release/numaBenchmarkTPCH'
@@ -12,20 +23,17 @@ STATUS_FILE = 'current_run.status'
 OUTPUT_FILE = 'current_run.out'
 NAME_FILE = 'current_run.name'
 
-
-SCALE = 0.1
-# SCALE = 1
-# ITERATIONS_PER_QUERY = 300
+SCALE = 1
 QUERY_RUNS_PER_CORE = 3
 ITERATIONS_OVERALL = 3
 
-APPENDIX = '_cachemiss_cycleslost'
+APPENDIX = ''
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 RESULT_DIR_NAME = 'tpch_scale' + str(SCALE) + '_runspercore' + str(QUERY_RUNS_PER_CORE) + APPENDIX + '_' + timestamp
 RESULT_DIR = os.path.join(RESULT_DIR_BASE, RESULT_DIR_NAME)
 
-core_counts = list(range(80, 0, -5)) + [4, 3, 2, 1, 0]
-# core_counts = list(range(80, 0, -5)) + [1, 0]
+# core_counts = list(range(80, 0, -5)) + [4, 3, 2, 1, 0]
+core_counts = list(range(224, 0, -14)) + [7, 1, 0]
 # core_counts = list(range(20, -1, -1))
 # core_counts = [10]
 
@@ -70,8 +78,8 @@ for iteration in range(ITERATIONS_OVERALL):
                 '--runs', str(runs_per_query),
                 '--scheduler=' + use_scheduler,
                 '--cores', str(core_count),
-                '--pcm',
-                # '-q', '3',
+                # '--pcm',
+                # '-q', '6', '-q', '13'
                 '-q', '1', '-q', '3', '-q', '5', '-q', '6', '-q', '7', '-q', '9', '-q', '10', '-q', '13',
                 ]
             run(args, cwd=CWD, stdout=output, stderr=output)
@@ -81,3 +89,5 @@ with open(STATUS_FILE, 'a+') as status:
     status.write('Done\n')
 with open(NAME_FILE, 'w') as name_file:
     name_file.write(RESULT_DIR + '\n')
+
+bot_sendtext('Benchmarks complete!')
