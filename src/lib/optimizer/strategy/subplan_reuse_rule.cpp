8db23e71d18d5b59564bba32872fae745a7ebeef
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2019-10-20 12:36:33 +0200

    schon wieder ein blocker

--- src/lib/optimizer/strategy/subplan_reuse_rule.cpp
+++ src/lib/optimizer/strategy/subplan_reuse_rule.cpp
@@ -2,8 +2,9 @@
 
 #include <unordered_map>
 
-#include "expression/lqp_column_expression.hpp"
+#include "expression/binary_predicate_expression.hpp"
 #include "expression/expression_utils.hpp"
+#include "expression/lqp_column_expression.hpp"
 #include "logical_query_plan/abstract_lqp_node.hpp"
 #include "logical_query_plan/lqp_utils.hpp"
 
@@ -12,9 +13,7 @@ namespace {
 using namespace opossum;  // NOLINT
 
 struct LQPHash {
-  bool operator()(const std::shared_ptr<AbstractLQPNode>& root) const {
-    return root->hash();
-  }
+  bool operator()(const std::shared_ptr<AbstractLQPNode>& root) const { return root->hash(); }
 };
 
 struct LQPEquals {
@@ -29,14 +28,16 @@ struct SubplanReplacement {
   std::shared_ptr<AbstractLQPNode> replacement_plan;
 };
 
-using SubplanEqualityMapping = std::unordered_map<std::shared_ptr<AbstractLQPNode>, std::vector<std::shared_ptr<AbstractLQPNode>>, LQPHash, LQPEquals>;
+using SubplanEqualityMapping = std::unordered_map<std::shared_ptr<AbstractLQPNode>,
+                                                  std::vector<std::shared_ptr<AbstractLQPNode>>, LQPHash, LQPEquals>;
 using SubplanReplacementMapping = std::vector<SubplanReplacement>;
 using ColumnReplacementMapping = std::unordered_map<LQPColumnReference, LQPColumnReference>;
 
-std::unordered_set<LQPColumnReference> get_column_references(const AbstractLQPNode& node) {
+std::unordered_set<LQPColumnReference> get_column_references(
+    const std::vector<std::shared_ptr<AbstractExpression>>& expressions) {
   auto column_references = std::unordered_set<LQPColumnReference>{};
-  for (const auto& node_expression : node.column_expressions()) {
-    visit_expression(node_expression, [&](const auto& sub_expression) {
+  for (const auto& expression : expressions) {
+    visit_expression(expression, [&](const auto& sub_expression) {
       if (const auto column_expression = std::dynamic_pointer_cast<LQPColumnExpression>(sub_expression)) {
         column_references.emplace(column_expression->column_reference);
       }
@@ -46,24 +47,36 @@ std::unordered_set<LQPColumnReference> get_column_references(const AbstractLQPNo
   return column_references;
 }
 
-bool expressions_contain_any_column_reference(const std::vector<std::shared_ptr<AbstractExpression>>& expressions,
-    const std::unordered_set<LQPColumnReference>& column_reference_blacklist) {
-  auto conflict = false;
-  for (const auto& expression : expressions) {
-    visit_expression(expression, [&](const auto& sub_expression) {
-      if (const auto column_expression = std::dynamic_pointer_cast<LQPColumnExpression>(sub_expression)) {
-        if (column_reference_blacklist.count(column_expression->column_reference)) {
-          conflict = true;
-          return ExpressionVisitation::DoNotVisitArguments;
-        }
-      }
-      return ExpressionVisitation::VisitArguments;
-    });
+ColumnReplacementMapping create_column_mapping(const AbstractLQPNode& from_node, const AbstractLQPNode& to_node) {
+  auto mapping = std::unordered_map<LQPColumnReference, LQPColumnReference>{};
+
+  const auto& from_expressions = from_node.column_expressions();
+  const auto& to_expressions = to_node.column_expressions();
+
+  Assert(from_expressions.size() == to_expressions.size(), "Expected same number of expressions");
+
+  const auto traverse_expressions = [&](const auto& expression_a, const auto& expression_b) {
+    if (!expression_a) {
+      return;
+    }
+
+    Assert(expression_a->type == expression_b->type, "Expected same type");
+
+    if (const auto column_expression_a = std::dynamic_pointer_cast<LQPColumnExpression>(expression_a)) {
+      const auto column_expression_b = std::dynamic_pointer_cast<LQPColumnExpression>(expression_b);
+      mapping.emplace(column_expression_a->column_reference, column_expression_b->column_reference);
+    }
+  };
+
+  for (auto column_id = ColumnID{0}; column_id < from_expressions.size(); ++column_id) {
+    traverse_expressions(from_expressions[column_id], to_expressions[column_id]);
   }
-  return conflict;
+
+  return mapping;
 }
 
-void add_column_replacements(ColumnReplacementMapping& column_replacements, const AbstractLQPNode& obsolete_plan, const AbstractLQPNode& replacement_plan) {
+void add_column_replacements(ColumnReplacementMapping& column_replacements, const AbstractLQPNode& obsolete_plan,
+                             const AbstractLQPNode& replacement_plan) {
   const auto& obsolete_expressions = obsolete_plan.column_expressions();
   const auto& replacements_expressions = replacement_plan.column_expressions();
 
@@ -87,74 +100,146 @@ void add_column_replacements(ColumnReplacementMapping& column_replacements, cons
   }
 }
 
-void traverse(
-    const std::shared_ptr<AbstractLQPNode>& parent,
-    const LQPInputSide input_side,
-    std::unordered_set<std::shared_ptr<AbstractLQPNode>>& visited,
-    const std::unordered_set<LQPColumnReference>& column_blacklist,
-    const SubplanEqualityMapping& equal_subplans,
-    SubplanReplacementMapping& subplan_mapping) {
+void apply_column_replacement_mapping(std::shared_ptr<AbstractExpression>& expression,
+                                      const ColumnReplacementMapping& column_replacement_mapping) {
+  visit_expression(expression, [&](auto& sub_expression) {
+    if (const auto column_expression = std::dynamic_pointer_cast<LQPColumnExpression>(sub_expression)) {
+      const auto column_replacement_iter = column_replacement_mapping.find(column_expression->column_reference);
+      if (column_replacement_iter != column_replacement_mapping.end()) {
+        sub_expression = std::make_shared<LQPColumnExpression>(column_replacement_iter->second);
+      }
+    }
+    return ExpressionVisitation::VisitArguments;
+  });
+}
 
-  const auto input = parent->input(input_side);
-  if (!input) {
+void apply_column_replacement_mapping(std::vector<std::shared_ptr<AbstractExpression>>& expressions,
+                                      const ColumnReplacementMapping& column_replacement_mapping) {
+  for (auto& expression : expressions) {
+    apply_column_replacement_mapping(expression, column_replacement_mapping);
+  }
+}
+
+bool is_join_predicate_ambiguous_with_reuse(const JoinNode& join_node,
+                                            const ColumnReplacementMapping& column_replacement_mapping) {
+  /**
+   * TODO: Only rewrite expressions from the input side that we're coming from
+   */
+
+  auto expressions_left = expressions_deep_copy(join_node.left_input()->column_expressions());
+  auto expressions_right = expressions_deep_copy(join_node.right_input()->column_expressions());
+
+  apply_column_replacement_mapping(expressions_left, column_replacement_mapping);
+  apply_column_replacement_mapping(expressions_right, column_replacement_mapping);
+
+  const auto is_join_predicate_ambiguous = [&](const auto& predicate) {
+    const auto binary_predicate = std::dynamic_pointer_cast<BinaryPredicateExpression>(predicate);
+    Assert(binary_predicate, "Expected join predicate to be binary");
+
+    const auto find_column_id = [](const auto& column_expressions, const auto& expression) {
+      for (auto column_id = ColumnID{0}; column_id < column_expressions.size(); ++column_id) {
+        if (*column_expressions[column_id] == expression) return true;
+      }
+      return false;
+    };
+
+    const auto left_in_left = find_column_id(expressions_left, *binary_predicate->arguments[0]);
+    const auto left_in_right = find_column_id(expressions_right, *binary_predicate->arguments[0]);
+    const auto right_in_left = find_column_id(expressions_left, *binary_predicate->arguments[1]);
+    const auto right_in_right = find_column_id(expressions_right, *binary_predicate->arguments[1]);
+
+    // Predicate is ambiguous when: No argument refers to an input side OR multiple arguments refer to an input
+    if (left_in_left && right_in_right) {
+      return left_in_right || right_in_left;
+    } else if (left_in_right && right_in_left) {
+      return left_in_left || right_in_right;
+    } else {
+      return true;
+    }
+  };
+
+  for (const auto& join_predicate : join_node.join_predicates()) {
+    auto rewritten_join_predicate = join_predicate->deep_copy();
+    apply_column_replacement_mapping(rewritten_join_predicate, column_replacement_mapping);
+    if (is_join_predicate_ambiguous(rewritten_join_predicate)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool is_join_output_reduced_with_reuse(const JoinNode& join_node,
+                                       const ColumnReplacementMapping& column_replacement_mapping) {
+  auto column_expressions = expressions_deep_copy(join_node.column_expressions());
+  const auto pre_distinct_column_count = get_column_references(column_expressions).size();
+
+  apply_column_replacement_mapping(column_expressions, column_replacement_mapping);
+  const auto post_distinct_column_count = get_column_references(column_expressions).size();
+
+  return pre_distinct_column_count != post_distinct_column_count;
+}
+
+bool is_reuse_possible(const std::shared_ptr<AbstractLQPNode>& parent, const LQPInputSide input_side,
+                       const std::shared_ptr<AbstractLQPNode>& replacement) {
+  auto result = true;
+
+  const auto candidate = parent->input(input_side);
+
+  std::cout << "Testing replacement of \n" << *candidate << " with\n " << *replacement << std::endl;
+
+  const auto column_mapping = create_column_mapping(*candidate, *replacement);
+
+  visit_lqp_upwards(parent, [&](const auto& node) {
+    if (const auto join_node = std::dynamic_pointer_cast<JoinNode>(node)) {
+      if (join_node->join_mode != JoinMode::Cross) {
+        if (is_join_predicate_ambiguous_with_reuse(*join_node, column_mapping)) {
+          std::cout << "NOT POSSIBLE BECAUSE OF PREDICATE of "<< join_node->description() << std::endl;
+          result = false;
+        }
+      }
+
+      if (is_join_output_reduced_with_reuse(*join_node, column_mapping)) {
+        std::cout << "NOT POSSIBLE BECAUSE OF OUTPUT of " << join_node->description() << std::endl;
+        result = false;
+      }
+
+      if (result) std::cout << "OKAY WITH " << join_node->description() << std::endl;
+
+      return result ? LQPVisitation::VisitInputs : LQPVisitation::DoNotVisitInputs;
+    }
+
+    return LQPVisitation::VisitInputs;
+  });
+
+  return result;
+}
+
+void traverse(const std::shared_ptr<AbstractLQPNode>& parent, const LQPInputSide input_side,
+              std::unordered_set<std::shared_ptr<AbstractLQPNode>>& visited,
+              const SubplanEqualityMapping& equal_subplans, SubplanReplacementMapping& subplan_mapping) {
+  const auto node = parent->input(input_side);
+  if (!node) {
     return;
   }
 
-  if (!visited.emplace(input).second) {
-    return;
-  }
-
-  const auto candidate_plans_iter = equal_subplans.find(input);
+  const auto candidate_plans_iter = equal_subplans.find(node);
   Assert(candidate_plans_iter != equal_subplans.end(), "Expected an entry for each subplan");
-
   const auto candidate_plans = candidate_plans_iter->second;
 
   for (const auto& candidate_plan : candidate_plans) {
-    if (candidate_plan == input) {
+    if (candidate_plan == node) {
       break;
     }
 
-    if (expressions_contain_any_column_reference(candidate_plan->column_expressions(), column_blacklist)) {
-      continue;
+    if (is_reuse_possible(parent, input_side, candidate_plan)) {
+      subplan_mapping.emplace_back(SubplanReplacement{parent, input_side, candidate_plan});
+      return;
     }
-
-    subplan_mapping.emplace_back(SubplanReplacement{parent, input_side, candidate_plan});
-    return;
   }
 
-  if (const auto join_node = std::dynamic_pointer_cast<JoinNode>(input)) {
-    for (const auto next_input_side : {LQPInputSide::Left, LQPInputSide::Right}) {
-      const auto opposite_input_side = next_input_side == LQPInputSide::Left ? LQPInputSide::Right : LQPInputSide::Left;
-
-      switch (join_node->join_mode) {
-        case JoinMode::Semi:
-        case JoinMode::AntiNullAsTrue:
-        case JoinMode::AntiNullAsFalse:
-          if (next_input_side == LQPInputSide::Right) {
-            auto next_column_blacklist = get_column_references(*join_node->input(LQPInputSide::Left));
-            traverse(input, next_input_side, visited, next_column_blacklist, equal_subplans, subplan_mapping);
-            break;
-          } else {
-            [[fallthrough]];
-          }
-
-        case JoinMode::Inner:
-        case JoinMode::Left:
-        case JoinMode::Right:
-        case JoinMode::FullOuter:
-        case JoinMode::Cross: {
-          auto next_column_blacklist = column_blacklist;
-          const auto opposite_input_column_references =
-              get_column_references(*join_node->input(opposite_input_side));
-          next_column_blacklist.insert(opposite_input_column_references.begin(), opposite_input_column_references.end());
-          traverse(input, next_input_side, visited, next_column_blacklist, equal_subplans, subplan_mapping);
-        } break;
-      }
-    }
-  } else {
-    traverse(input, LQPInputSide::Left, visited, column_blacklist, equal_subplans, subplan_mapping);
-    traverse(input, LQPInputSide::Right, visited, column_blacklist, equal_subplans, subplan_mapping);
-  }
+  traverse(node, LQPInputSide::Left, visited, equal_subplans, subplan_mapping);
+  traverse(node, LQPInputSide::Right, visited, equal_subplans, subplan_mapping);
 }
 
 }  // namespace
@@ -169,7 +254,7 @@ void SubplanReuseRule::apply_to(const std::shared_ptr<AbstractLQPNode>& root) co
   visit_lqp(root, [&](const auto& subplan) {
     auto equal_subplans_iter = equal_subplans.find(subplan);
     if (equal_subplans_iter == equal_subplans.end()) {
-      equal_subplans.emplace(subplan, SubplanEqualityMapping::mapped_type{});
+      equal_subplans.emplace(subplan, SubplanEqualityMapping::mapped_type{subplan});
       return LQPVisitation::VisitInputs;
     } else {
       equal_subplans_iter->second.emplace_back(subplan);
@@ -182,9 +267,8 @@ void SubplanReuseRule::apply_to(const std::shared_ptr<AbstractLQPNode>& root) co
    * Find possible reuses
    */
   auto visited_nodes = std::unordered_set<std::shared_ptr<AbstractLQPNode>>{};
-  const auto column_blacklist = get_column_references(*root);
   auto subplan_replacements = SubplanReplacementMapping{};
-  traverse(root, LQPInputSide::Left, visited_nodes, column_blacklist, equal_subplans, subplan_replacements);
+  traverse(root, LQPInputSide::Left, visited_nodes, equal_subplans, subplan_replacements);
 
   if (subplan_replacements.empty()) {
     return;
@@ -205,7 +289,7 @@ void SubplanReuseRule::apply_to(const std::shared_ptr<AbstractLQPNode>& root) co
    * Fix all expressions that referred to now-removed subplans
    */
   visit_lqp(root, [&](const auto& node) {
-    for(auto& node_expression : node->node_expressions) {
+    for (auto& node_expression : node->node_expressions) {
       visit_expression(node_expression, [&](auto& sub_expression) {
         if (const auto column_expression = std::dynamic_pointer_cast<LQPColumnExpression>(sub_expression)) {
           const auto column_replacement_iter = column_replacements.find(column_expression->column_reference);
--- src/test/optimizer/strategy/subplan_resuse_rule_test.cpp
+++ src/test/optimizer/strategy/subplan_resuse_rule_test.cpp
@@ -29,7 +29,32 @@ class SubplanReuseRuleTest : public StrategyBaseTest {
   std::shared_ptr<SubplanReuseRule> rule;
 };
 
-TEST_F(SubplanReuseRuleTest, Basic) {
+TEST_F(SubplanReuseRuleTest, BasicReuseMadePossibleByAggregate) {
+  // clang-format off
+  const auto input_lqp =
+  PredicateNode::make(greater_than_(sum_(a_a), 10),
+    JoinNode::make(JoinMode::Semi, equals_(sum_(a_a), b_a),
+      AggregateNode::make(expression_vector(), expression_vector(sum_(a_a)),
+        mock_node_a),
+      mock_node_b));
+
+  const auto expected_lqp =
+  PredicateNode::make(greater_than_(sum_(b_a), 10),
+    JoinNode::make(JoinMode::Semi, equals_(sum_(b_a), b_a),
+      AggregateNode::make(expression_vector(), expression_vector(sum_(b_a)),
+        mock_node_b),
+      mock_node_b));
+  // clang-format on
+
+  std::cout << *input_lqp << std::endl;
+
+  const auto actual_lqp = apply_rule(rule, input_lqp);
+  std::cout << *actual_lqp << std::endl;
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(SubplanReuseRuleTest, BasicReuseMadePossibleBySemiJoin) {
   // clang-format off
   const auto input_lqp =
   PredicateNode::make(greater_than_(a_a, 10),
@@ -55,24 +80,45 @@ TEST_F(SubplanReuseRuleTest, Basic) {
 
 TEST_F(SubplanReuseRuleTest, NoReuse) {
   // clang-format off
-  const auto input_lqp =
-  JoinNode::make(JoinMode::Inner, equals_(a_a, b_a),
+  const auto input_lqp_a =
+  JoinNode::make(JoinMode::Semi, equals_(b_a, c_a),
+    mock_node_b,
+    mock_node_c);
+
+  const auto input_lqp_b =
+    JoinNode::make(JoinMode::Semi, equals_(b_a, c_a),
+       mock_node_b,
+       mock_node_c);
+
+  const auto input_lqp_c =
+  JoinNode::make(JoinMode::Cross,
     mock_node_a,
     JoinNode::make(JoinMode::Inner, equals_(b_a, c_a),
       mock_node_b,
       mock_node_c));
 
-  const auto expected_lqp =
-  JoinNode::make(JoinMode::Inner, equals_(a_a, b_a),
-    mock_node_a,
-    JoinNode::make(JoinMode::Inner, equals_(b_a, c_a),
-      mock_node_b,
-      mock_node_c));
+  const auto input_lqp_d =
+  PredicateNode::make(greater_than_(a_a, 10),
+    JoinNode::make(JoinMode::Inner, equals_(sum_(a_a), b_a),
+      AggregateNode::make(expression_vector(), expression_vector(sum_(a_a)),
+        mock_node_a),
+      mock_node_b));
+
+  const auto expected_lqp_a = input_lqp_a->deep_copy();
+  const auto expected_lqp_b = input_lqp_b->deep_copy();
+  const auto expected_lqp_c = input_lqp_c->deep_copy();
+  const auto expected_lqp_d = input_lqp_d->deep_copy();
   // clang-format on
 
-  const auto actual_lqp = apply_rule(rule, input_lqp);
+  const auto actual_lqp_a = apply_rule(rule, input_lqp_a);
+  const auto actual_lqp_b = apply_rule(rule, input_lqp_b);
+  const auto actual_lqp_c = apply_rule(rule, input_lqp_c);
+  const auto actual_lqp_d = apply_rule(rule, input_lqp_d);
 
-  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+  EXPECT_LQP_EQ(actual_lqp_a, expected_lqp_a);
+  EXPECT_LQP_EQ(actual_lqp_b, expected_lqp_b);
+  EXPECT_LQP_EQ(actual_lqp_c, expected_lqp_c);
+  EXPECT_LQP_EQ(actual_lqp_d, expected_lqp_d);
 }
 
 }  // namespace opossum
